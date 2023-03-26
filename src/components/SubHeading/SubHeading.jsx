871cedb82c9f6cfcfe9130dda1ac50357cdca7b3
bipro <biprocuet@gmail.com>
2023-03-26 11:20:27 +0600

    Gallery

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -4,7 +4,7 @@ const SubHeading = ({ title }) => {
   return (
     <div style={{ marginButton: "1rem" }}>
       <p className="p__cormorant">{title}</p>
-      <img src={images.spoon} alt="spoon_imahe" className="spoon__img" />
+      <img src={images.spoon} alt="spoon_image" className="spoon__img" />
     </div>
   );
 };
